5497a159482dfa99a90e3f1f27ea1cf7153d1334
appleVovan <yablonskyi.v.reg@gmail.com>
2021-01-20 23:36:36 +0200

    Less code and less readable

--- src/Budgets/Program.cs
+++ src/Budgets/Program.cs
@@ -6,7 +6,7 @@ namespace KMA.ProgrammingInCSharp2021.Budgets
     {
         static void Main(string[] args)
         {
-            Console.WriteLine((args.Length > 0) ? $"Hello, {args[0]}!" : "Hello!");
+            Console.WriteLine($"Hello{((args.Length > 0)? $", {args[0]}" : "")}!");
             if (args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[0]}!");
