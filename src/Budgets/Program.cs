cd507412fb41fec2e1a77a716e0c4899f86628ce
appleVovan <yablonskyi.v.reg@gmail.com>
2021-01-21 14:08:01 +0200

    revrt of previous change

--- src/Budgets/Program.cs
+++ src/Budgets/Program.cs
@@ -6,7 +6,7 @@ namespace KMA.ProgrammingInCSharp2021.Budgets
     {
         static void Main(string[] args)
         {
-            if (args.Length > 0);
+            if (args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[0]}!");
             }
