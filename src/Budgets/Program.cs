0b42291231886e9635288b8018254553416ea53d
appleVovan <yablonskyi.v.reg@gmail.com>
2021-01-21 14:08:31 +0200

    float x

--- src/Budgets/Program.cs
+++ src/Budgets/Program.cs
@@ -6,6 +6,7 @@ namespace KMA.ProgrammingInCSharp2021.Budgets
     {
         static void Main(string[] args)
         {
+            float x;
             if (args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[0]}!");
